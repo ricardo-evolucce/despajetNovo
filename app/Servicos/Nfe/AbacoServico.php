e3ae101ec083177d79f3c6fcc74d8884d030f5f3
ricardo <ricardo.evolucce@gmail.com>
2021-05-07 11:42:53 -0300

    numero de serie nota agora tem alfanumerico adicionado

--- app/Servicos/Nfe/AbacoServico.php
+++ app/Servicos/Nfe/AbacoServico.php
@@ -54,9 +54,42 @@ class AbacoServico extends Servico
 
 		return [
 			'numero' => $maior_num+$numero_adicao,
-			'serie' => $maior_serie+$serie_adicao
+			'serie' => $this->alphanumeric_increment($maior_serie, $position = false);
 		];
 	}
+
+
+
+	function alphanumeric_increment( $string, $position=false ) {
+        if ( false === $position ) {
+                $position = strlen( $string ) - 1;
+        }
+        $increment_str = substr( $string, $position, 1 );
+        switch ( $increment_str ) {
+                case '9':
+                        $string = substr_replace( $string, 'a', $position, 1 );
+                        break;
+                case 'z':
+                        $string = substr_replace( $string, 'A', $position, 1 );
+                        break;
+                case 'Z':
+                        if ( 0 === $position ) {
+                                $string = substr_replace( $string, '0', $position, 1 );
+                                $string .= '0';
+                        } else {
+                                $inc_position = $position - 1;
+                                $string = alphanumeric_increment( $string, $inc_position );
+                                $string = substr_replace( $string, '0', $position, 1 );
+                        }
+                        break;
+                default:
+                        $increment_str++;
+                        $string = substr_replace( $string, $increment_str, $position, 1 );
+                        break;
+        }
+        return $string;
+}
+
 
 	public function transmitir($nfs_assinado)
 	{
